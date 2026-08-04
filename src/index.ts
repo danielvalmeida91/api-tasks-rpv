845a8d6661feed44d688094dfceb17bf35450c34
Daniel Ventura de Almeida <danielvalmeida91@gmail.com>
2026-08-03 22:10:17 -0300

    feat: creating first endpoints

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,28 @@ app.get("/healthy", (req, res) => {
     res.status(200).json({ message: "Server is healthy" });
 })
 
+app.post("/task", (req, res) => {
+    const { title, description, status_id, priority_id, category_id } = req.body;
+
+    // 1. Validar o body da requisição ( zod validação de entrada )
+    // 2. Insere no banco 
+    // 3. Retorna status 201, com as infomações criadas
+
+    return res.status(201).json({ message: "Task created successfully", data: { title, description, status_id, priority_id, category_id } });
+})
+
+app.get("/task/:id", (req, res) => {
+    // 1. Verificar se o id enviado existe no banco 
+    // 2. Retorna erro em caso de nao existir
+    // 3. Retorna as informações se existir
+})
+
+app.patch("/task/:id", (req, res) => {
+    // 1. Verificar se o id enviado existe no banco 
+    // 2. Retorna erro em caso de nao existir
+    // 3. Atualiza as informações conforme body da requisição
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
